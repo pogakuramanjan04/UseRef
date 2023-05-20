9e898f554f230d36f9837791eca61aec7904bbe0
Pogaku Ram <125488490+pogakuramanjan04@users.noreply.github.com>
2023-05-20 09:43:29 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ export default function App() {
   const inputRef = useRef(null);
   const inputRef1 = useRef(null);
   const [data, setData] = useState("");
-  console.log(inputRef1.current)
- 
+  console.log(inputRef1.current);
+
   // console.log(inputRef1.current.innerText)
-  
 
   return (
     <div className="App">
@@ -20,8 +19,10 @@ export default function App() {
         onChange={() => setData(inputRef.current.value)}
       />
       <h3>{data}</h3>
-      <h3 ref={inputRef1}>sdjbjbj</h3>
-      <button onClick={() => inputRef1.current.innerText = "lime"}>Change Second Paragraph Background Color</button>
+      <h3 ref={inputRef1}>Ram</h3>
+      <button onClick={() => (inputRef1.current.innerText = "RAMU")}>
+        click
+      </button>
     </div>
   );
 }
